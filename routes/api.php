c3fa0cbfd36be89dcd4106cdd70a92c276b25665
Shadman-Ahmed-Chowdhury <shadmanchowdhury87@gmail.com>
2021-07-04 00:13:09 +0600

    Add model and controller with resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::resource('products', ProductController::class);
+
+// Route::get('/products', [ProductController::class, 'index']);
+
+// Route::post('/product/create', [ProductController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
