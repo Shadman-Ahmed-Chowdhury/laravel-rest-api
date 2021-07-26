a1dbb372e48abb7ea76cdafbabf7e94c10cdf34a
Shadman-Ahmed-Chowdhury <shadmanchowdhury87@gmail.com>
2021-07-26 10:32:43 +0600

    register and logout functionality added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -16,8 +17,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController::class);
+// Route::resource('products', ProductController::class);
+//Public Routes
+Route::post('/register', [AuthController::class, 'register']); 
+Route::get('/products', [ProductController::class, 'index']); 
+Route::get('/products/{id}', [ProductController::class, 'show']); 
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+//Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']); 
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);  
+    Route::post('/logout', [AuthController::class, 'logout']); 
+});
 
 // Route::get('/products', [ProductController::class, 'index']);
 
